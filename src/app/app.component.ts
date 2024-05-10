014196b9cb799b62085bddecf7dc0933b7cf0004
ThanhNguyen03 <thanhnguyen25011207@gmail.com>
2024-05-11 03:30:58 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: '../styles.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WeatherApp';
+  constructor(private weatherService: WeatherService) {
+
+  } 
+
+  cityName: string = 'Ho Chi Minh'
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response)
+      },
+      error: (error) => {
+        console.error('Error fetching weather data:', error);
+      }
+    })
+  }
 }
